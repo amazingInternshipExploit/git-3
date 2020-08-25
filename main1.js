ec6ba11d776010117d3f4ff1b45b428067f96d3d
Intership Student <shinobi_2@shinobi_2.intership.yonder.ninja>
2020-08-25 19:19:52 +0200

    first page

--- main1.js
+++ main1.js
@@ -1,69 +1,22 @@
-$(window).load(function(){
-     $('.preloader').fadeOut('slow');
-});
-
-
-/* =Main INIT Function
--------------------------------------------------------------- */
-function initializeSite() {
-
-	"use strict";
-
-	//OUTLINE DIMENSION AND CENTER
-	(function() {
-	    function centerInit(){
-
-			var sphereContent = $('.sphere'),
-				sphereHeight = sphereContent.height(),
-				parentHeight = $(window).height(),
-				topMargin = (parentHeight - sphereHeight) / 2;
-
-			sphereContent.css({
-				"margin-top" : topMargin+"px"
-			});
-
-			var heroContent = $('.hero'),
-				heroHeight = heroContent.height(),
-				heroTopMargin = (parentHeight - heroHeight) / 2;
-
-			heroContent.css({
-				"margin-top" : heroTopMargin+"px"
-			});
-
-	    }
-
-	    $(document).ready(centerInit);
-		$(window).resize(centerInit);
-	})();
-
-	// Init effect 
-	$('#scene').parallax();
-
-};
-/* END ------------------------------------------------------- */
-
-/* =Document Ready Trigger
--------------------------------------------------------------- */
-$(window).load(function(){
-
-	initializeSite();
-	(function() {
-		setTimeout(function(){window.scrollTo(0,0);},0);
-	})();
-
-});
-/* END ------------------------------------------------------- */
-
-
-$('#countdown').countdown({
-	date: "December 13, 2017 18:03:26",
-	render: function(data) {
-	  var el = $(this.el);
-	  el.empty()
-	    //.append("<div>" + this.leadingZeros(data.years, 4) + "<span>years</span></div>")
-	    .append("<div>" + this.leadingZeros(data.days, 2) + " <span>days</span></div>")
-	    .append("<div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div>")
-	    .append("<div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div>")
-	    .append("<div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
-	}
-});
+<html>
+  <head>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <style>
+      body {
+        padding-top: 50px
+      }
+    </style>
+  </head>
+  <body>
+  <div class="container">
+      <div class="panel panel-warning">
+        <div class="panel-heading">
+          <h3 class="panel-title">Docker Flow Proxy: 503 Service Unavailable</h3>
+        </div>
+      <div class="panel-body">
+        No server is available to handle this request.
+      </div>
+  </div>
+</body>
+</html>
