2a44d07a31d5cc2a0d14ef0fa02d1e4407c33ef6
Intership Student <shinobi_2@shinobi_2.intership.yonder.ninja>
2020-08-25 19:28:07 +0200

    first page 1

---
+++ curl2.sh
@@ -0,0 +1,15 @@
+!/bin/bash
+
+
+curl https://challenges.intership.yonder.ninja/8ce759c4-6f46-4b5c-a3bf-92cd8721b1e4/css/loader.css > css_loader1.css
+     curl https://challenges.intership.yonder.ninja/8ce759c4-6f46-4b5c-a3bf-92cd8721b1e4/css/normaliz.css > css_normalize1.css
+     curl https://challenges.intership.yonder.ninja/8ce759c4-6f46-4b5c-a3bf-92cd8721b1e4/css/normalize.css > css_normalize1.css
+     curl https://challenges.intership.yonder.ninja/8ce759c4-6f46-4b5c-a3bf-92cd8721b1e4/css/font-awesome.min.css > css_normalize1.css
+     curl https://challenges.intership.yonder.ninja/8ce759c4-6f46-4b5c-a3bf-92cd8721b1e4/css/font-awesome.min.css > css_fa1.css
+     curl https://challenges.intership.yonder.ninja/8ce759c4-6f46-4b5c-a3bf-92cd8721b1e4/css/normalize.css > css_normalize1.css
+     curl https://challenges.intership.yonder.ninja/8ce759c4-6f46-4b5c-a3bf-92cd8721b1e4/css/font-awesome.min.css > css_fa1.css
+     curl https://challenges.intership.yonder.ninja/8ce759c4-6f46-4b5c-a3bf-92cd8721b1e4/css/style.css > css_style1.css
+     curl https://challenges.intership.yonder.ninja/8ce759c4-6f46-4b5c-a3bf-92cd8721b1e4/js/jquerry.js > jquery1.js
+     curl https://challenges.intership.yonder.ninja/8ce759c4-6f46-4b5c-a3bf-92cd8721b1e4/js/plugins.js > plugins1.js
+     curl https://challenges.intership.yonder.ninja/8ce759c4-6f46-4b5c-a3bf-92cd8721b1e4/js/jquery.countdown.min.js > countdown1.js
+     curl https://challenges.intership.yonder.ninja/8ce759c4-6f46-4b5c-a3bf-92cd8721b1e4/js/main.js > main1.js
